b758963862c7f4c2dfa502a30d4a4facf2602c1d
Aarjav Jain <anshjain638@gmail.com>
2024-09-13 23:03:20 +0530

    setup the bottom tab navigator

---
+++ components/Navigation/BottomTabNavigation.tsx
@@ -0,0 +1,69 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import AddLead from '../../screens/AddLead';
+import Home from '../../screens/Home';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import {StatusBar} from 'react-native';
+
+export type BottomTabParamList = {
+  Home: undefined;
+  Add: undefined;
+};
+
+const Tab = createBottomTabNavigator<BottomTabParamList>();
+
+export default function BottomTabNavigator() {
+  return (
+    <>
+      <StatusBar
+        barStyle={'dark-content'}
+        backgroundColor="transparent"
+        translucent={true}
+      />
+      <Tab.Navigator
+        backBehavior="order"
+        screenOptions={{
+          tabBarActiveBackgroundColor: 'aqua',
+          tabBarStyle: {
+            backgroundColor: 'lightgray',
+            height: 60,
+            alignItems: 'center',
+            justifyContent: 'center',
+          },
+        }}>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: 'All Leads',
+            tabBarLabel: 'Leads',
+            headerRight: () => (
+              <Icon
+                name="download"
+                size={20}
+                color="blue"
+                style={{marginRight: 10}}
+                onPress={() => console.log('Download Leads')}
+              />
+            ),
+            tabBarIcon: ({focused}) => (
+              <Icon name="list" size={20} color={focused ? 'blue' : 'black'} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Add"
+          component={AddLead}
+          options={{
+            title: 'Add Lead',
+            tabBarLabel: 'Add',
+            tabBarIcon: ({focused}) => (
+              <Icon name="plus" size={20} color={focused ? 'blue' : 'black'} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </>
+  );
+}
